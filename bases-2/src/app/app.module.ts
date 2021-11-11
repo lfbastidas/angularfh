168cc5cea7cb274797c61c91742e60e080f56cbd
lfbastidas <luis.bastidas@horisoft.co>
2021-11-11 15:32:10 -0500

    Bases 2 primer commit

---
+++ bases-2/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+//Componentes
+import { AppComponent } from './app.component';
+//Modulos
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent, 
+  ],
+  imports: [
+    BrowserModule,
+    HeroesModule,
+    ContadorModule,
+    DbzModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
